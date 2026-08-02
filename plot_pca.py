8af6220e9f80add158e89109b70b65e6e13a5fc2
Annalise Lariviere Brown <alarivierebr@gmail.com>
2026-08-02 02:11:49 +0100

    Testing group by breed

--- plot_pca.py
+++ plot_pca.py
@@ -2,14 +2,56 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Read eigenvectors
-pca = pd.read_csv("/mnt/autofs/data/userdata/project0076/annalise/filtering/plink_files_pca/cohort_pca.eigenvec", sep=r"\s+")
+# pca = pd.read_csv("/mnt/autofs/data/userdata/project0076/annalise/filtering/plink_files_pca/unfiltered/cohort_pca.eigenvec", sep=r"\s+")
+# print(pca.columns.tolist())
+# #exit()
+
+# plt.figure(figsize=(8,6))
+# #plt.scatter(pca["PC1"], pca["PC2"], s=12)
+
+# metadata = pd.read_csv("/mnt/autofs/data/userdata/project0076/annalise/filtering/plink_files_pca/metadata.csv")
+
+# pca = pca.merge(metadata, on="IID")
+# print(pca.columns.tolist())
+# print(pca.groupby(by="Breed").groups)
+
+# for IID, Breed in pca.groupby(pca.Breed):
+#     plt.scatter(Breed["PC1"], Breed["PC2"], label="Breed")
+
+
+# plt.xlabel("PC1")
+# plt.ylabel("PC2")
+# plt.title("Unfiltered cohort PCA")
+
+# plt.tight_layout()
+# plt.savefig("cohort_pca.png", dpi=300)
+
+#--------------------------------
+
+# Read eigenvectors
+pca = pd.read_csv("/mnt/autofs/data/userdata/project0076/annalise/filtering/plink_files_pca/filtered/filtered_cohort_pca.eigenvec", sep=r"\s+")
+print(pca.columns.tolist())
+#exit()
 
 plt.figure(figsize=(8,6))
-plt.scatter(pca["PC1"], pca["PC2"], s=12)
+#plt.scatter(pca["PC1"], pca["PC2"], s=12)
+
+metadata = pd.read_csv("/mnt/autofs/data/userdata/project0076/annalise/filtering/plink_files_pca/metadata.csv")
+
+pca = pca.merge(metadata, on="IID")
+print(pca.columns.tolist())
+print(pca.groupby(by="Breed").groups)
+
+for IID, Breed in pca.groupby(pca.Breed):
+    plt.scatter(Breed["PC1"], Breed["PC2"], label="Breed")
+    print(IID)
+
 
 plt.xlabel("PC1")
 plt.ylabel("PC2")
-plt.title("Unfiltered cohort PCA")
+plt.title("Filtered cohort PCA")
 
 plt.tight_layout()
-plt.savefig("cohort_pca.png", dpi=300)
+plt.savefig("filtered_cohort_pca.png", dpi=300)
+
+#Do I want it to be grouped by Breed, or ony colored by breed, but each point is still individual?????
